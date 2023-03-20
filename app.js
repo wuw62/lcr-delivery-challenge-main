8d3221df19b08fbf0080750b46e2a82f83ef0a87
Weiyi Wu <itsrealwaiiwoo@gmail.com>
2023-03-20 15:07:46 -0400

    card function update

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import { cardData } from "./cardData.js"
 
-const retrieveData = () => {
-    console.log(cardData[0]);
+const gstPercentage = 0.13;
+const cost = document.getElementById("total-cost");
+const miles = document.getElementById("total-miles");
+const gst = document.getElementById("GST-HST");
+
+const retrieveData = (index) => {
+    cost.innerHTML = cardData[index].cost * (1 + gstPercentage);
+    miles.innerHTML = cardData[index].amount;
+    gst.innerHTML = cardData[index].cost * gstPercentage;
 }
+
 const b1Amount = document.getElementById("b1_amount");
 const b2Amount = document.getElementById("b2_amount");
 const b3Amount = document.getElementById("b3_amount");
@@ -19,7 +27,12 @@ b1Cost.innerHTML = "$" + cardData[0].cost;
 b2Cost.innerHTML = "$" + cardData[1].cost;
 b3Cost.innerHTML = "$" + cardData[2].cost;
 
-
 const b1 = document.getElementById("b1");
-b1.addEventListener("click", retrieveData);
+b1.addEventListener("click", () => retrieveData(0));
+
+const b2 = document.getElementById("b2");
+b2.addEventListener("click", () => retrieveData(1));
+
+const b3 = document.getElementById("b3");
+b3.addEventListener("click", () =>retrieveData(2));
 
