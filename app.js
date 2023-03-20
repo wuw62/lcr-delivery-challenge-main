205fabe876a5c7699ebf970688e3605b13721b0f
Weiyi Wu <itsrealwaiiwoo@gmail.com>
2023-03-20 14:39:47 -0400

    Repost

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const b1Cost = document.getElementById("b1_cost");
 const b2Cost = document.getElementById("b2_cost");
 const b3Cost = document.getElementById("b3_cost");
 
+b1Amount.innerHTML = "Buy " + cardData[0].amount + "Milies";
+b2Amount.innerHTML = "Buy " + cardData[1].amount + "Milies";
+b3Amount.innerHTML = "Buy " + cardData[2].amount + "Milies";
+
 b1Cost.innerHTML = "$" + cardData[0].cost;
 b2Cost.innerHTML = "$" + cardData[1].cost;
 b3Cost.innerHTML = "$" + cardData[2].cost;
 
-b1Amount.innerHTML = "Buy " + cardData[0].amount + "Milies";
-b2Amount.innerHTML = "Buy " + cardData[1].amount + "Milies";
-b3Amount.innerHTML = "Buy " + cardData[2].amount + "Milies";
 
 const b1 = document.getElementById("b1");
 b1.addEventListener("click", retrieveData);
